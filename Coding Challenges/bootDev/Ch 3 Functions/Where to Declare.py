86840a7ef667999486c3f3922281cf2c45b1c31d
Victor Gonzalez <vagonzalezs0511@gmail.com>
2024-01-27 08:28:07 -0600

    Added Exercises from boot.dev

---
+++ Coding Challenges/bootDev/Ch 3 Functions/Where to Declare.py
@@ -0,0 +1,14 @@
+''' 
+The main() function is a convention used in many programming languages 
+to specify the entry point of an application. By defining a single main 
+function, and only calling main() at the end of the entire program we 
+ensure that all of our functions are defined before they're called.
+
+'''
+
+def main():
+    print("Fantasy Quest is booting up...")
+    print("Game is running!")
+
+main()
+
