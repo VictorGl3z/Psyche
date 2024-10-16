453479e7f79ca0f520339b83f4370d0ae9eaa436
Victor Gonzalez <vagonzalezs0511@gmail.com>
2024-10-15 21:11:27 -0600

    Challenge Completed

---
+++ Coding Challenges/Edabit/Python/2 - Convert Minutes into Seconds/CC-2 Solution 1.py
@@ -0,0 +1,21 @@
+# Problem Statement 
+# Create a function that takes an integer minutes and converts it to seconds.
+# Examples
+# convert(5) -> 300
+# convert(3) -> 180
+
+def convert(minutes):
+    seconds = minutes * 60
+    return seconds
+
+while True:
+    mins = input('Input your minutes: ')
+    try:
+        float(mins)
+        break
+    except:
+        print('This is not a valid number!')
+        continue
+
+mins = float(mins)
+print(mins, 'mins are:', convert(mins), 'seconds.')
