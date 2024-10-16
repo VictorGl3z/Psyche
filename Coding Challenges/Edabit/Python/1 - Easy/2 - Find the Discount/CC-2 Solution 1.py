672964e192c6eca5502758b54772ecfa19ae1d7f
Victor Gonzalez <vagonzalezs0511@gmail.com>
2024-10-16 11:08:03 -0600

    Challenge Completed

---
+++ Coding Challenges/Edabit/Python/1 - Easy/2 - Find the Discount/CC-2 Solution 1.py
@@ -0,0 +1,31 @@
+# Problem Statement
+# Create a function that takes two arguments: The original price and the discount percentage as integers
+# and returns the final price after the discount.
+# Examples
+# dis(1500,50) -> 750
+# dis(100,75) -> 25
+
+def dis(price, discount):
+    total = price - price * discount / 100
+    return round(total,2)
+    
+while True:
+    og = input('Type the original price of the product: ')
+    try:
+        og = int(og)
+        break
+    except:
+        print('Please type a valid number!')
+        continue
+
+while True:
+    perc = input('Type the discount percentage: ')
+    try:
+        perc = int(perc)
+        break
+    except:
+        print('Please type a valid number!')
+        continue
+
+
+print('The final price after the discount is:',dis(og,perc))
