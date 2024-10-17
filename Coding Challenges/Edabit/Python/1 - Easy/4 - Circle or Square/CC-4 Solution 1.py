8dfc64deb353d68f9959d29a8b281bed5751e593
Victor Gonzalez <vagonzalezs0511@gmail.com>
2024-10-17 15:07:12 -0600

    Challenge Completed

---
+++ Coding Challenges/Edabit/Python/1 - Easy/4 - Circle or Square/CC-4 Solution 1.py
@@ -0,0 +1,44 @@
+# Problem Statement
+# Given the radius of a circle and the area of a square, return True if the circumference of the circe is greater than the square's perimeter
+# and False if the square's perimeter is greater than the circumference of the circle
+# Examples
+# circle_or_square(16, 625) -> True
+# circle_or_square(5, 100) -> false
+
+import math 
+
+def circle_or_square(r_circle, a_square):
+    circ = math.pi * r_circle * 2
+    square = math.sqrt(a_square) * 4
+    if circ > square:
+        res = True
+    else:
+        res = False
+    return res
+
+while True:
+    radius = input('Type the radius of the circle: ')
+    try:
+        radius = float(radius)
+        break
+    except:
+        print('That is not a valid number!')
+        continue
+
+while True:
+    area = input('Type the area of the square: ')
+    try:
+        area = float(area)
+        break
+    except:
+        print('That is not a valid number!')
+        continue
+    
+returned = circle_or_square(radius, area)
+if returned:
+    print('The circumference of the circle is greater than the perimeter of the square.')
+else:
+    print('The area of the square is greater than the circumference of the circle.')
+    
+
+    
